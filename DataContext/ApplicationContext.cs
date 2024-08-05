31ba94f6c13fe3cd02a514706427923cf1c10696
Leonardo <leeo_skw@hotmail.com>
2024-08-05 19:19:53 -0300

    Add project files.

---
+++ DataContext/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace estudos_api_relacionamentos.DataContext
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tabela> nomeTabela { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+
+using estudos_api_relacionamentos.DataContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace estudos_api_relacionamentos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            ConfiguraBd(builder.Services, builder.Configuration);
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+
+        static void ConfiguraBd(IServiceCollection services, ConfigurationManager configuration)
+        {
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseMySql(configuration.GetConnectionString("MySqlConnectionString"), new MySqlServerVersion(new Version(8, 0, 21))));
+        }
+    }
+}
