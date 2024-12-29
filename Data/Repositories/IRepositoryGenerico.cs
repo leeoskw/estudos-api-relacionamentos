966d8553150bae8a8923c310e17560ea11291940
Leonardo <leeo_skw@hotmail.com>
2024-12-29 19:15:44 -0300

    :sparkles: feat: implementa repositório genérico

---
+++ Data/Repositories/IRepositoryGenerico.cs
@@ -0,0 +1,11 @@
+﻿namespace estudos_api_relacionamentos.Data.Repositories
+{
+    public interface IRepositoryGenerico<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Data/Repositories/RepositoryGenerico.cs
@@ -0,0 +1,44 @@
+﻿using estudos_api_relacionamentos.DataContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace estudos_api_relacionamentos.Data.Repositories
+{
+    public class RepositoryGenerico<T> : IRepositoryGenerico<T> where T : class
+    {
+        private readonly ApplicationContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public RepositoryGenerico(ApplicationContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public void Update(T entity)
+        {
+             _dbSet.Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
